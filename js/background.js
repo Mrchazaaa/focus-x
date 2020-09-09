96b918621b0b332343061dfcb0ee72ba84163f0e
charlie <charlie.howlett@live.co.uk>
2020-09-09 21:29:05 +0100

    Added cached tracking of focused elements when devtools is closed

--- js/background.js
+++ js/background.js
@@ -1,4 +1,35 @@
+const maxMessageQueueLength = 50;
 var connections = {};
+
+function createConnection(port) {
+    var newConnection = {
+        messageQueue: [],
+        flushMessages: function() {
+            var request = this.messageQueue.shift();
+            while(request) {
+                this.port.postMessage(request);
+                request = this.messageQueue.shift();
+            }
+        },
+        postMessage: function(request) {
+            if (this.messageQueue.length == maxMessageQueueLength) {
+                this.messageQueue.shift();
+            }
+
+            this.messageQueue.push(request);
+
+            if (this.port) {
+                this.flushMessages();
+            }
+        }
+    };
+
+    if (port) {
+        newConnection.port = port;
+    }
+
+    return newConnection;
+}
 
 chrome.runtime.onConnect.addListener(function (port) {
 
@@ -6,7 +37,12 @@ chrome.runtime.onConnect.addListener(function (port) {
         // The original connection event doesn't include the tab ID of the
         // DevTools page, so we need to send it explicitly.
         if (message.tabId) {
-            connections[message.tabId] = port;
+            if (connections[message.tabId]) {
+                connections[message.tabId].port = port;
+                connections[message.tabId].flushMessages();
+            } else {
+                connections[message.tabId] = createConnection(port);
+            }
             return;
         }
     }
@@ -19,10 +55,10 @@ chrome.runtime.onConnect.addListener(function (port) {
 
         var tabs = Object.keys(connections);
         for (var i=0, len=tabs.length; i < len; i++) {
-          if (connections[tabs[i]] == port) {
-            delete connections[tabs[i]]
-            break;
-          }
+            if (connections[tabs[i]].port == port) {
+                delete connections[tabs[i]].port;
+                break;
+            }
         }
     });
 });
@@ -31,10 +67,17 @@ chrome.runtime.onConnect.addListener(function (port) {
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     // Messages from content scripts should have sender.tab set
     if (sender.tab) {
-      var tabId = sender.tab.id;
-      if (tabId in connections) {
-        connections[tabId].postMessage(request);
-      }
+        var tabId = sender.tab.id;
+        if (tabId in connections) {
+            connections[tabId].postMessage(request);
+        } else {
+            connections[tabId] = createConnection();
+            connections[tabId].postMessage(request);
+        }
     }
     return true;
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId, removed) {
+    delete connections[tabId];
 });
